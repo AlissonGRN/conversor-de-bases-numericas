e65fe922beb0551bdfa171effca691ce3c4b0038
alisson guilherme <guilhermeribeiro099@gmail.com>
2019-10-03 15:02:01 -0300

    sistema de conversão de decimal para binario/octal/hexadecimal

--- conversor.py
+++ conversor.py
@@ -1,3 +1,28 @@
 # conversor de bases numericas
-# autores: Alisson Guilherme, Samuel Silva
+# autor: Alisson Guilherme
 
+print('Conversor de bases numericas')
+print('-'*30)
+
+print('''Qual base numerica deseja utilizar ?
+Decimal     [ 1 ] 
+Binario     [ 2 ]
+Octal       [ 3 ]
+Hexadecimal [ 4 ]
+''')
+opcao = int(input("opção: "))
+
+if opcao == 1:
+    numDecimal = int((input('Digite um numero decimal: ')))
+    print('convertido para binario: {}'.format(bin(numDecimal)))
+    print('convertido para hexadecimal: {}'.format(hex(numDecimal)))
+    print('convertido para octal: {}'.format(oct(numDecimal)))
+
+elif opcao == 2:
+    pass
+elif opcao == 3:
+    pass
+elif opcao == 4:
+    pass
+else:
+    print('opção não recomhecida')
